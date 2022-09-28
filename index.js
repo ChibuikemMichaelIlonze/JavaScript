4771f98b850ab9df848f7e6e9dd23a7a902e62ae
Chibuikem Michael Ilonze <56983788+ChibuikemMichaelIlonze@users.noreply.github.com>
2022-09-28 17:01:28 +0100

    edit

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ function hoopCount(n) {
   if (n >= 10) return "Great, now move on to tricks";
   else return "Keep at it until you get it";
 }
+//for counting total number of goals of a player
 function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   // code goes here
   return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
